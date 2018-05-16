788427a4dfb74ce8f84c139e1b1bb3d7393f6ed0
Andres Villalobose <aevr007@gmail.com>
2018-05-16 17:53:15 -0400

    Fixed a condition, missing test command on the concatenation

--- manifests/python.pp
+++ manifests/python.pp
@@ -4,20 +4,20 @@ class ts_sal::python{
 	exec{ "get-custom-python":
 		path => "/bin:/usr/bin/:/usr/sbin" ,
 		command => "wget ftp://ftp.noao.edu/pub/dmills/python3-to-install.tgz -O /tmp/python3-to-install.tgz",
-		onlyif => "test ! -f /tmp/python3-to-install.tgz  && ! -f /usr/local/bin/python3.6"
+		onlyif => "test ! -f /tmp/python3-to-install.tgz  && test ! -f /usr/local/bin/python3.6"
 	}
 	exec{ 'unpack-custom-python':
 		path => "/bin:/usr/bin/:/usr/sbin" ,
 		command => "cd /tmp/ && gunzip python3-to-install.tgz && tar xvf python3-to-install.tar",
 		require => Exec['get-custom-python'],
-		onlyif => "test ! -f /tmp/python3-to-install.tar  && ! -f /usr/local/bin/python3.6"
+		onlyif => "test ! -f /tmp/python3-to-install.tar  && test ! -f /usr/local/bin/python3.6"
 	}
 	
 	exec{ 'install-custom-python':
 		path => "/bin:/usr/bin/:/usr/sbin" ,
 		command => "cd /tmp/Python-3.6.3/ && make install",
 		require => Exec['unpack-custom-python'],
-		onlyif => "test ! -d /tmp/Python-3.6.3/  && ! -f /usr/local/bin/python3.6"
+		onlyif => "test ! -d /tmp/Python-3.6.3/  && test ! -f /usr/local/bin/python3.6"
 	}
 	
 	exec{ 'install-numpy':
