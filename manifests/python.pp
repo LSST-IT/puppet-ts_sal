7ce0ab8f26df038b006cc2ccff20519aa0b5c943
Andres Villalobose <aevr007@gmail.com>
2018-05-16 18:01:40 -0400

    Condition mistake on instal-custom-python

--- manifests/python.pp
+++ manifests/python.pp
@@ -17,14 +17,14 @@ class ts_sal::python{
 		path => "/bin:/usr/bin/:/usr/sbin" ,
 		command => "cd /tmp/Python-3.6.3/ && make install",
 		require => Exec['unpack-custom-python'],
-		onlyif => "test ! -d /tmp/Python-3.6.3/  && test ! -f /usr/local/bin/python3.6"
+		onlyif => "test -d /tmp/Python-3.6.3/  && test ! -f /usr/local/bin/python3.6"
 	}
 	
 	exec{ 'install-numpy':
 		path => "/bin/:/usr/bin:/usr/sbin/:/usr/local/bin/",
 		command => "pip3 install numpy",
 		require => Exec['install-custom-python'],
-		onlyif => "test -z \"$(pip3 list --format=columns 2>/dev/null | grep numpy)\""
+		onlyif => "test -z \"$(/usr/local/bin/pip3 list --format=columns 2>/dev/null | grep numpy)\""
 	}
 	
 }
