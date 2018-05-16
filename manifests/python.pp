3ef667bc6510677af91958455dccc271bf36de8d
Andres Villalobose <aevr007@gmail.com>
2018-05-16 14:52:55 -0400

    Adding verifications in python installation in such a way that the commands are executed only if python insn't installed.

--- manifests/python.pp
+++ manifests/python.pp
@@ -4,26 +4,27 @@ class ts_sal::python{
 	exec{ "get-custom-python":
 		path => "/bin:/usr/bin/:/usr/sbin" ,
 		command => "wget ftp://ftp.noao.edu/pub/dmills/python3-to-install.tgz -O /tmp/python3-to-install.tgz",
-		onlyif => "test ! -f /tmp/python3-to-install.tgz "
+		onlyif => "test ! -f /tmp/python3-to-install.tgz  && ! -f /usr/local/bin/python3.6"
 	}
 	exec{ 'unpack-custom-python':
 		path => "/bin:/usr/bin/:/usr/sbin" ,
 		command => "cd /tmp/ && gunzip python3-to-install.tgz && tar xvf python3-to-install.tar",
 		require => Exec['get-custom-python'],
-		onlyif => "test -f /tmp/python3-to-install.tgz "
+		onlyif => "test ! -f /tmp/python3-to-install.tar  && ! -f /usr/local/bin/python3.6"
 	}
 	
 	exec{ 'install-custom-python':
 		path => "/bin:/usr/bin/:/usr/sbin" ,
 		command => "cd /tmp/Python-3.6.3/ && make install",
 		require => Exec['unpack-custom-python'],
-		onlyif => "test -d /tmp/Python-3.6.3/"
+		onlyif => "test ! -d /tmp/Python-3.6.3/  && ! -f /usr/local/bin/python3.6"
 	}
 	
 	exec{ 'install-numpy':
 		path => "/bin/:/usr/bin:/usr/sbin/:/usr/local/bin/",
 		command => "pip3 install numpy",
 		require => Exec['install-custom-python'],
+		onlyif => "test -z \"$(pip3 list --format=columns 2>/dev/null | grep numpy)\""
 	}
 	
 }
